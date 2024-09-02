ed0932b44d9d3bd71ab93f03753be13808cb101f
cisse410 <dramisse90@gmail.com>
2024-09-02 23:48:33 +0000

    adding others entities

---
+++ src/main/java/sn/cisse410/models/Lecture.java
@@ -0,0 +1,24 @@
+package sn.cisse410.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
